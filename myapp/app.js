ea7bd6f429fdc25f5593cfe3534cead71a7bf9d9
Harshita <harshitas234btechcse@kccitm.edu.in>
2022-06-14 01:12:59 +0530

    jqueryy

--- myapp/app.js
+++ myapp/app.js
@@ -5,11 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var studnetRouter = require('./routes/student');
+// var usersRouter = require('./routes/users');
+var studentRouter = require('./routes/student');
 var teacherRouter = require('./routes/teacher');
 var loginRouter = require('./routes/login');
-var idcardRouter= require('./routes/icard');
+var idcardRouter= require('./routes/idcard');
 var app = express();
 
 // view engine setup
@@ -23,9 +23,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/studnet', studnetRouter);
+app.use('/student', studentRouter);
 app.use('/teacher', teacherRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/idcard', idcardRouter);
 // catch 404 and forward to error handler
