3d0a2e86727877cc4e0746f1bcd98ad1221fec06
Harshita <harshitas234btechcse@kccitm.edu.in>
2022-06-14 11:35:08 +0530

    13junesql

--- myapp/model/login.js
+++ myapp/model/login.js
@@ -1,9 +1,9 @@
-export var a = (d)=>{
-    console.log("iraf is dating "+d)
-}
+// export var a = (d)=>{
+//     console.log("iraf is dating "+d)
+// }
 
-export const pandy = {
-    name: 'pandy',
-    lname: 'pandy',
-    constact:[{phoneNo:98,address:"galli no 420"}]
-}
+// export const pandy = {
+//     name: 'pandy',
+//     lname: 'pandy',
+//     constact:[{phoneNo:98,address:"galli no 420"}]
+// }
