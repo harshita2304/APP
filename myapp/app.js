a3f7edfd525ead40a7c07ab562087b299f4e693d
Harshita <harshitas234btechcse@kccitm.edu.in>
2022-06-23 16:02:02 +0530

    calculatorr

--- myapp/app.js
+++ myapp/app.js
@@ -11,6 +11,7 @@ var teacherRouter = require('./routes/teacher');
 var loginRouter = require('./routes/login');
 var idcardRouter= require('./routes/idcard');
 var calRouter= require('./routes/cal');
+//var norRouter = require('./routes/nor');
 var app = express();
 
 // view engine setup
@@ -24,12 +25,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/student', studentRouter);
-app.use('/teacher', teacherRouter);
+//app.use('/student', studentRouter);
+//app.use('/teacher', teacherRouter);
 // app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/idcard', idcardRouter);
 app.use('/cal', calRouter);
+//app.use('/nor', norRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
